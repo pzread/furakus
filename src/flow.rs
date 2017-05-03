a8d1af64fefd14ba2de495875c024288b2d2764f
PZ Read <netfirewall@gmail.com>
2017-05-04 00:53:23 +0800

    Improve tests

--- src/flow.rs
+++ src/flow.rs
@@ -370,15 +370,30 @@ mod tests {
     fn waiting_push() {
         let ptr = Flow::new(None);
 
-        for idx in 0..(MAX_CAPACITY / MAX_SIZE as u64) {
+        sync_assert_eq!(ptr.write().unwrap().push(b"A"), Ok(0));
+        for idx in 1..(MAX_CAPACITY / MAX_SIZE as u64) {
             sync_assert_eq!(ptr.write().unwrap().push(&[0u8; MAX_SIZE]), Ok(idx));
         }
         let base_idx = MAX_CAPACITY / MAX_SIZE as u64;
+        sync_assert_eq!(ptr.write().unwrap().push(b"D"), Ok(base_idx));
 
-        let fut = ptr.write().unwrap().push(b"B");
-        sync_assert_eq!(ptr.write().unwrap().push(&[0u8; MAX_SIZE]), Err(Error::Invalid));
-        sync_assert_eq!(ptr.read().unwrap().pull(0, Some(0)), Ok(vec![0u8; MAX_SIZE]));
-        sync_assert_eq!(fut, Ok(base_idx));
-        sync_assert_eq!(ptr.write().unwrap().push(b"C"), Ok(base_idx + 1));
+        let fut = ptr.write().unwrap().push(&[0u8; MAX_SIZE]);
+        sync_assert_eq!(ptr.read().unwrap().pull(0, Some(0)), Ok(Vec::from(b"A" as &[u8])));
+        sync_assert_eq!(ptr.read().unwrap().pull(1, Some(0)), Ok(vec![0u8; MAX_SIZE]));
+        sync_assert_eq!(fut, Ok(base_idx + 1));
+
+        ptr.write().unwrap().push(&[0u8; MAX_SIZE]);
+        sync_assert_eq!(ptr.write().unwrap().push(b"C"), Err(Error::Invalid));
+    }
+
+    #[test]
+    fn outlive() {
+        let fut = {
+            let ptr = Flow::new(None);
+            sync_assert_eq!(ptr.write().unwrap().push(b"A"), Ok(0));
+            let flow = ptr.read().unwrap();
+            flow.pull(0, Some(0))
+        };
+        sync_assert_eq!(fut, Err(Error::Other));
     }
 }
